016066e2d0fddb5de81005312b8c50fd1639c897
Gladwinnnn <gladwin1503@hotmail.com>
2020-08-29 22:12:18 +0800

    last question of homework

--- Rational.java
+++ Rational.java
@@ -1,4 +1,59 @@
 
 public class Rational {
+    private int numerator;
+    private int denominator;
 
+    public Rational(){
+        numerator = 1;
+        denominator = 1;
+    }
+
+    public Rational(int numerator, int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator(){
+        return numerator;
+    }
+
+    public int getDenominator(){
+        return denominator;
+    }
+
+    public void setNumerator(int numerator){
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator){
+        this.denominator = denominator;
+    }
+
+    public Rational add(Rational secondNumber){
+        int tempNumerator = this.numerator * secondNumber.getDenominator() + secondNumber.getNumerator() * this.denominator;
+        int tempDenominator = this.denominator * secondNumber.getDenominator();
+        return new Rational(tempNumerator, tempDenominator);
+    }
+
+    public Rational subtract(Rational secondNumber){
+        int tempNumerator = this.numerator * secondNumber.getDenominator() - secondNumber.getNumerator() * this.denominator;
+        int tempDenominator = this.denominator * secondNumber.getDenominator();
+        return new Rational(tempNumerator, tempDenominator);
+    }
+
+    public Rational multiply(Rational secondNumber){
+        int tempNumerator = this.numerator * secondNumber.getNumerator();
+        int tempDenominator = this.denominator * secondNumber.getDenominator();
+        return new Rational(tempNumerator, tempDenominator);
+    }
+
+    public Rational divide(Rational secondNumber){
+        int tempNumerator = this.numerator * secondNumber.getDenominator();
+        int tempDenominator = this.denominator * secondNumber.getNumerator();
+        return new Rational(tempNumerator, tempDenominator);
+    }
+
+    public String toString(){
+        return this.getNumerator() + "/" + this.getDenominator();
+    }
 }
